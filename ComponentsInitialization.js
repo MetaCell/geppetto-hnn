6877cb341b7704e5cbaee1236a3f1de94f6af8ae
Afonso Pinto <afonsobpinto@gmail.com>
2018-10-20 16:24:29 +0100

    Add skeleton

---
+++ ComponentsInitialization.js
@@ -0,0 +1,55 @@
+define(function (require) {
+    return function (GEPPETTO) {
+        var ReactDOM = require('react-dom');
+        var React = require('react');
+        var MuiThemeProvider = require('material-ui/styles/MuiThemeProvider').default;
+        var HNNMain = require('./HNNMain').default;
+        var injectTapEventPlugin = require('react-tap-event-plugin');
+
+        var Utils = require('./Utils').default;
+        var Console = require('../../js/components/interface/console/Console');
+        var TabbedDrawer = require('../../js/components/interface/drawer/TabbedDrawer');
+        var PythonConsole = require('../../js/components/interface/pythonConsole/PythonConsole');
+
+        require('./css/hnn.less');
+        require('./css/material.less');
+
+        injectTapEventPlugin();
+
+        function App(data = {}) {
+            return (
+                <div>
+                    <MuiThemeProvider>
+                        <HNNMain/>
+                    </MuiThemeProvider>
+
+                    <div id="footer">
+                        <div id="footerHeader">
+                            <TabbedDrawer labels={["Console", "Python"]} iconClass={["fa fa-terminal", "fa fa-flask"]} >
+                                <Console />
+                                <PythonConsole pythonNotebookPath={"http://" + window.location.hostname + ":" + window.location.port + "/notebooks/notebook.ipynb"} />
+                            </TabbedDrawer>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        ReactDOM.render(<App />, document.querySelector('#mainContainer'));
+
+        GEPPETTO.G.setIdleTimeOut(-1);
+        GEPPETTO.G.debug(true); //Change this to true to see messages on the Geppetto console while loading
+        GEPPETTO.Resources.COLORS.DEFAULT = "#008ea0";
+        GEPPETTO.trigger(GEPPETTO.Events.Show_spinner, "Initialising HNN");
+
+
+        GEPPETTO.on('jupyter_geppetto_extension_ready',  (data) => {
+            Utils.execPythonMessage('from hnn_ui.hnn_geppetto import hnn_geppetto');
+            Utils.evalPythonMessage('hnn_geppetto.getData',[]).then((response) => {
+                //FIXME: Hack to remove backslashes manually
+                var data = JSON.parse(response.replace(/\\/g, "/"))
+                ReactDOM.render(<App data={data} />, document.querySelector('#mainContainer'));
+                GEPPETTO.trigger("spinner:hide");
+            })
+        });
+    };
+});
