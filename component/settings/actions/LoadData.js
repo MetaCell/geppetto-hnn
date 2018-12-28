c7b5daebfd85de16714b86fe2bc32b58016e7647
Afonso Pinto <afonsobspinto@gmail.com>
2018-12-28 18:12:50 +0000

    Add drag and drop upload

--- component/settings/actions/LoadData.js
+++ component/settings/actions/LoadData.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DialogContentText from "@material-ui/core/es/DialogContentText/DialogContentText";
 import Button from '@material-ui/core/Button';
 import { withStyles } from "@material-ui/core";
+import Dropzone from "react-dropzone";
 import FileBrowser from '../../general/FileBrowser';
 import AlertDialog from './AlertDialog';
 
@@ -17,8 +18,19 @@ class LoadData extends React.Component {
 	state = {
 		explorerDialogOpen: false,
 		exploreOnlyDirs: false,
-
+		files: []
 	};
+
+
+	onDrop(files) {
+		this.setState({ files });
+	}
+
+	onCancel() {
+		this.setState({
+			files: []
+		});
+	}
 
 
 	showExplorerDialog (exploreOnlyDirs) {
@@ -28,7 +40,7 @@ class LoadData extends React.Component {
 	closeExplorerDialog (fieldValue) {
 		const newState = { explorerDialogOpen: false };
 		if (fieldValue) {
-			let fileName = fieldValue.path.replace(/^.*[\\\/]/, '');
+			let fileName = fieldValue.path.replace(/^.*[\\]/, '');
 			let path = fieldValue.path.split(fileName).slice(0, -1).join('');
 			newState["modFolder"] = fieldValue.path;
 			newState["modPath"] = path;
@@ -39,13 +51,34 @@ class LoadData extends React.Component {
 	render () {
 		const { classes, ...others } = this.props;
 		const { explorerDialogOpen, exploreOnlyDirs } = this.state;
+		const files = this.state.files.map(file => (
+			<li key={file.name}>
+				{file.name} - {file.size} bytes
+			</li>
+		))
+
 		return (
 			<AlertDialog
 				title="Load Experimental Data"
 				{...others}
 			>
 				<DialogContentText>
-					Drop file here or <Button color="secondary" className={classes.button} onClick={() => this.showExplorerDialog(true)}> Click Here To Upload </Button>
+					<Dropzone
+						onDrop={this.onDrop.bind(this)}
+						onFileDialogCancel={this.onCancel.bind(this)}
+					>
+						{({ getRootProps, getInputProps }) => (
+							<div {...getRootProps()}>
+								<input {...getInputProps()} />
+								<p>Drop file here or <Button color="secondary" className={classes.button} onClick={() => this.showExplorerDialog(true)}> Click Here To Upload </Button></p>
+							</div>
+						)}
+					</Dropzone>
+					<aside>
+						<h4>Files</h4>
+						<ul>{files}</ul>
+					</aside>
+
 				</DialogContentText>
 
 				<FileBrowser
