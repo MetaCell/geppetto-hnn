a1603f5b50764bc256fb9e726ac3666fa12fe42e
rodriguez-facundo <bruseghini.f@gmail.com>
2019-02-22 10:26:52 -0300

    move to geppetto-application instead of org.geppetto.frontend

--- ComponentsInitialization.js
+++ ComponentsInitialization.js
@@ -1,50 +1,52 @@
-define(function (require) {
-  return function (GEPPETTO) {
-    let React = require('react');
-    let ReactDOM = require('react-dom');
-    let MuiThemeProvider = require('@material-ui/core/styles/MuiThemeProvider').default;
-    let Utils = require('./Utils').default;
-    let HNNMain = require('./HNNMain').default;
-    let theme = require('./Theme').default;
-    let Console = require('../../js/components/interface/console/Console');
-    let TabbedDrawer = require('../../js/components/interface/drawer/TabbedDrawer');
-    let PythonConsole = require('../../js/components/interface/pythonConsole/PythonConsole');
-        
-    require('./css/hnn.less');
+global.jQuery = require("jquery");
+global.GEPPETTO_CONFIGURATION = require('./GeppettoConfiguration.json');
 
-    function App (data = {}) {
-      return (
-        <div>
-          <MuiThemeProvider theme={theme}>
-            <HNNMain {...data} />
-          </MuiThemeProvider>
+jQuery(function () {
+  require('geppetto-client-initialization');
+  const React = require('react');
+  const ReactDOM = require('react-dom');
+  const MuiThemeProvider = require('@material-ui/core/styles/MuiThemeProvider').default;
+  const Utils = require('./Utils').default;
+  const HNNMain = require('./HNNMain').default;
+  const theme = require('./Theme').default;
+  const Console = require('../../js/components/interface/console/Console');
+  const TabbedDrawer = require('../../js/components/interface/drawer/TabbedDrawer');
+  const PythonConsole = require('../../js/components/interface/pythonConsole/PythonConsole');
+      
+  require('./css/hnn.less');
 
-          <div id="footer" style={{visibility:"hidden"}}>
-            <div id="footerHeader">
-              <TabbedDrawer labels={["Console", "Python"]} iconClass={["fa fa-terminal", "fa fa-flask"]}>
-                <Console />
-                <PythonConsole pythonNotebookPath={"../notebooks/notebook.ipynb"} />
-              </TabbedDrawer>
-            </div>
+  function App (data = {}) {
+    return (
+      <div>
+        <MuiThemeProvider theme={theme}>
+          <HNNMain {...data} />
+        </MuiThemeProvider>
+
+        <div id="footer" style={{visibility:"hidden"}}>
+          <div id="footerHeader">
+            <TabbedDrawer labels={["Console", "Python"]} iconClass={["fa fa-terminal", "fa fa-flask"]}>
+              <Console />
+              <PythonConsole pythonNotebookPath={"../notebooks/notebook.ipynb"} />
+            </TabbedDrawer>
           </div>
         </div>
-      );
-    }
-    ReactDOM.render(<App />, document.querySelector('#mainContainer'));
+      </div>
+    );
+  }
+  ReactDOM.render(<App />, document.querySelector('#mainContainer'));
 
-    GEPPETTO.G.setIdleTimeOut(-1);
-    GEPPETTO.G.debug(true); // Change this to true to see messages on the Geppetto console while loading
-    GEPPETTO.Resources.COLORS.DEFAULT = "#008ea0";
-        
-    GEPPETTO.trigger(GEPPETTO.Events.Show_spinner, "Initialising HNN");
+  GEPPETTO.G.setIdconstimeOut(-1);
+  GEPPETTO.G.debug(true); // Change this to true to see messages on the Geppetto console while loading
+  GEPPETTO.Resources.COLORS.DEFAULT = "#008ea0";
+      
+  GEPPETTO.trigger(GEPPETTO.Events.Show_spinner, "Initialising HNN");
 
-    GEPPETTO.on('jupyter_geppetto_extension_ready', data => {
-      Utils.execPythonMessage('from hnn_ui.hnn_geppetto import hnn_geppetto');
-      Utils.evalPythonMessage('hnn_geppetto.getData',[]).then(response => {
-        let data = Utils.convertToJSON(response)
-        ReactDOM.render(<App data={data} />, document.querySelector('#mainContainer'));
-        GEPPETTO.trigger(GEPPETTO.Events.Hide_spinner);
-      })
-    });
-  };
+  GEPPETTO.on('jupyter_geppetto_extension_ready', data => {
+    Utils.execPythonMessage('from hnn_ui.hnn_geppetto import hnn_geppetto');
+    Utils.evalPythonMessage('hnn_geppetto.getData',[]).then(response => {
+      const data = Utils.convertToJSON(response)
+      ReactDOM.render(<App data={data} />, document.querySelector('#mainContainer'));
+      GEPPETTO.trigger(GEPPETTO.Events.Hide_spinner);
+    })
+  });
 });
