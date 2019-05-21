cb389eecbdeaa37fae441d1e978b0ff1b5794a09
rodriguez-facundo <bruseghini.f@gmail.com>
2019-05-21 11:02:03 -0300

    Order thumbnails manually

--- component/general/materialComponents/Thumbnail.js
+++ component/general/materialComponents/Thumbnail.js
@@ -17,19 +17,54 @@ const styles = {
   }
 }
 
-export default withStyles(styles)(({ names, selected, handleClick, variant = "round", classes }) => (
-  <div>
-    {names.map(name => (
-      <Fab
-        id={name}
-        key={name}
-        variant={variant}
-        onClick={ () => handleClick(name) }
-        color={ selected == name ? "primary" : "default" }
-        classes={{ root: classes[variant], label: classes[variant + "Label"] }}
-      >
-        {name}
-      </Fab>
-    ))}
-  </div>
-))
+export default withStyles(styles)(({ names, selected, handleClick, variant = "round", classes }) => {
+  let aux = false;
+  if (names && names.length) {
+    if (names[0] == "Layer 5" && names.length == 2) {
+      aux = Array(2)
+      const newOrder = [1, 0]
+      newOrder.forEach((i, index) => aux[index] = names[i])
+
+    } else if (names[0] == "Basal 1 dend" && names.length == 8){
+      aux = Array(8)
+      const newOrder = [1, 2, 4, 6, 7, 0, 5, 3];
+      newOrder.forEach((i, index) => aux[index] = names[i])
+    
+    } else if (names[0] == "Soma" && names.length == 2) {
+      // we got this one right
+      aux = names;
+
+    } else if (names[0] == "Layer 2/3 Basket" && names.length == 4) {
+      aux = Array(4)
+      const newOrder = [3, 0, 2, 1];
+      newOrder.forEach((i, index) => aux[index] = names[i])
+    
+    } else if (names[0] == "Timing" && names.length == 3) {
+      aux = Array(3)
+      const newOrder = [2, 1, 0];
+      newOrder.forEach((i, index) => aux[index] = names[i])
+    
+    } else {
+      aux = names
+    }  
+
+  } else {
+    aux = names
+  }
+  return (
+    <div>
+      {aux.map(name => (
+        <Fab
+          id={name}
+          key={name}
+          variant={variant}
+          onClick={ () => handleClick(name) }
+          color={ selected == name ? "primary" : "default" }
+          classes={{ root: classes[variant], label: classes[variant + "Label"] }}
+        >
+          {name}
+        </Fab>
+      ))}
+    </div>
+  )
+})
